ae49ca5ecf591c44f2cb562c08c618b5fc5761e2
marco-duart <marcoaurelio_db@hotmail.com>
2025-05-11 20:57:15 -0300

    feat: updated app with providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./assets/styles/global-styles";
+import { Toaster } from "react-hot-toast";
+import { theme } from "./assets/styles/theme";
+import { toastConfig } from "./config/toast-config";
+import Router from "./routes";
+import { UserProvider } from "./context/user-context";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <UserProvider>
+          <Toaster toastOptions={{ ...toastConfig }} />
+          <Router />
+        </UserProvider>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
