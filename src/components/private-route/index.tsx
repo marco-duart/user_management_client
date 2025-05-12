c8a7e6d253bd0433340a3e87a3ac7c06b23a3e59
marco-duart <marcoaurelio_db@hotmail.com>
2025-05-11 21:02:08 -0300

    feat: added private route component

---
+++ src/components/private-route/index.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useUserContext } from "../../hooks/use-user-context";
+import { useNavigate } from "react-router-dom";
+import { UserDTO } from "../../services/user/DTO";
+import { ReactNode } from "react";
+
+interface Props {
+  requiredRole: UserDTO.Role.USER | UserDTO.Role.ADMIN;
+  children?: ReactNode;
+}
+
+export const PrivateRoute = ({ requiredRole, children }: Props) => {
+  const { user } = useUserContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    } else if (user.role !== requiredRole) {
+      navigate("/access-denied");
+    }
+  }, [user, requiredRole, navigate]);
+
+  return user?.role === requiredRole ? <>{children}</> : null;
+};
